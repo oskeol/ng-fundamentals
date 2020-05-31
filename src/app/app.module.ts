e2593225e300eab587fcdfd72a46d24880b832ea
Oskeol Gjoni <oskeolgjoni@gmail.com>
2020-05-31 13:13:11 +0200

    ng-fundamentals project from the Pluralsight ng-fundamentals course.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+    EventListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver,
+    CreateSessionComponent,
+    SessionListComponent
+    } from './events/index';
+
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/toastr.service';
+
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './errors/404.component';
+import { UserModule } from './user/user.module';
+import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CollapsibleWellComponent } from './common/collapsible-well.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    UserModule
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    AuthService,
+    EventListResolver,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if(component.isDirty)
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  return true;
+}
---
+++ src/app/events/create-event.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventService } from './shared';
+
+@Component({
+  templateUrl: 'create-event.component.html',
+  styles: [`
+    em { float:right; color:#E05C65; padding-left:10px; }
+    .error input { background-color: #E3C3C5; }
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color: #999; }
+    .error :ms-input-placeholder { color: #999 }
+  `]
+})
+export class CreateEventComponent {
+  event: any;
+  isDirty: boolean = true;
+
+  constructor(private eventService: EventService, private router: Router){}
+
+  saveEvent(formValues){
+    this.eventService.saveEvent(formValues);
+    this.isDirty = false;
+    this.router.navigate(['/events']);
+  }
+
+  ngOnInit(){
+    this.event = {
+      name: 'Ng Spectacular',
+      date: '8/8/2028',
+      time: '10am',
+      price: 799.99,
+      location: {
+        address: '456 Happy St',
+        city: 'Felicity',
+        country: 'Angularistan'
+      },
+      onlineUrl: 'http://ngSpectacular.com',
+      imageUrl: 'http://ngSpectacular.com/logo.png'
+    }
+  }
+
+  cancel(){
+    this.router.navigate(['/events']);
+  }
+
+}
